3ebab6e1dd1c4963c42863f862b4364d6da7fdb0
Nick On <nick.on437@gmail.com>
2020-11-14 00:27:04 +1100

    New - Pulling raw data of 151 pokemon

---
+++ main.ts
@@ -0,0 +1,19 @@
+const fetchPokemon = () => {
+  // const abc = 2;
+  const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
+  const NUM_OF_POKEMON = 151;
+  const promises = [];
+
+  for (let i = 1; i <= NUM_OF_POKEMON; i++) {
+    const pokemonUrl = BASE_URL + i;
+    promises.push(fetch(pokemonUrl).then((result) => result.json()));
+  }
+
+  Promise.all(promises).then((results) => {
+    console.log(results);
+    // results.map((data) => {
+    // })
+  })
+};
+
+fetchPokemon();

74b9534897c337843d5bee178e6ea4387f492d0f
Nick On <nick.on437@gmail.com>
2020-11-14 16:42:06 +1100

    Style - Update background patterns

--- DetailView.ts
+++ DetailView.ts
@@ -16,13 +16,26 @@ const DetailView = () => {
       }
       const detailHtml = `
         <div id="overview">
+          <div class="background-patterns">
+            <img src="./resources/img/pokeball.svg" name="pokeball"/>
+            <svg width="154" height="215" name="dots-1">
+              <circle cx="46" cy="108" r="15"/>
+              <circle cx="108" cy="108" r="15"/>
+              <circle cx="46" cy="169" r="15"/>
+              <circle cx="108" cy="169" r="15"/>
+              <line x1="46" y1="46" x2="108" y2="46" stroke="currentColor" stroke-width="30" stroke-linecap="round"/>
+            </svg>
+            <svg x="0px" y="0px" viewBox="0 0 297.613 297.613" name="dots-2">
+              <g><circle cx="15.279" cy="14.83" r="14.83"/><circle cx="59.32" cy="59.769" r="14.83"/><circle cx="59.32" cy="14.83" r="14.83"/><circle cx="104.261" cy="59.769" r="14.83"/><circle cx="104.261" cy="103.81" r="14.83"/><circle cx="104.261" cy="14.83" r="14.83"/><circle cx="148.302" cy="59.769" r="14.83"/><circle cx="148.302" cy="103.81" r="14.83"/><circle cx="148.302" cy="14.83" r="14.83"/><circle cx="192.343" cy="59.769" r="14.83"/><circle cx="192.343" cy="103.81" r="14.83"/><circle cx="148.302" cy="147.852" r="14.83"/><circle cx="192.343" cy="147.852" r="14.83"/><circle cx="104.261" cy="192.79" r="14.831"/><circle cx="148.302" cy="192.79" r="14.831"/><circle cx="192.343" cy="192.79" r="14.831"/><circle cx="59.32" cy="236.887" r="14.83"/><circle cx="104.261" cy="236.887" r="14.83"/><circle cx="148.302" cy="236.887" r="14.83"/><circle cx="192.343" cy="236.887" r="14.83"/><circle cx="238.238" cy="103.81" r="14.83"/><circle cx="238.238" cy="147.852" r="14.83"/><circle cx="238.238" cy="192.79" r="14.831"/><circle cx="15.279" cy="282.782" r="14.831"/><circle cx="59.32" cy="282.782" r="14.831"/><circle cx="104.261" cy="282.782" r="14.831"/><circle cx="148.302" cy="282.782" r="14.831"/><circle cx="282.334" cy="147.852" r="14.83"/></g>
+            </svg>
+
+          </div>
           <img src="${data.sprites.other['official-artwork'].front_default}"></img>
           <div name="basic-data">
             <div class="pokeId">${pokeId}</div>
-              <div class="pokeName">${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</div>
-              <div class="pokeTypes hflex">
-                ${types}
-              </div>
+            <div class="pokeName">${data.name.charAt(0).toUpperCase() + data.name.slice(1)}</div>
+            <div class="pokeTypes hflex">
+              ${types}
             </div>
           </div>
         </div>

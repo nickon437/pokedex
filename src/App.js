c746c64742e052eeb50549e94b05751868f3fb0c
Nick On <nick.on437@gmail.com>
2020-11-20 21:49:53 +1100

    Fix - Style consistency issue on different screen size

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
   const fetchPokemon = useCallback(async () => {
     const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/';
-    const NUM_OF_POKEMON = 3;
+    const NUM_OF_POKEMON = 10;
     const promises = [];
     let pokemons;
     for (let i = 1; i <= NUM_OF_POKEMON; i++) {
@@ -39,6 +39,7 @@ const App = () => {
   // Save ctx to local storage
   useEffect(() => {
     window.localStorage.setItem('pokedex-context', JSON.stringify(ctxPokedex));
+    console.log('Upadte state');
   });
 
   return (

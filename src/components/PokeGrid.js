2cf8d7b151c7a5da66455f98631b03eae730a1e6
Nick On <nick.on437@gmail.com>
2020-11-25 23:14:10 +1100

    New - Add a back to generations view btn

--- src/components/PokeGrid.js
+++ src/components/PokeGrid.js
@@ -7,10 +7,26 @@ import './PokeGrid.scss';
 
 const PokeGrid = () => {
   const [ctxPokedex, setCtxPokedex] = useContext(PokedexContext);
-  const pokeData = ctxPokedex.filteredPokemons.map((pkm) => <PokeGridElement key={pkm.id} pkm={pkm}/>);  
+  const pokeData = ctxPokedex.filteredPokemons.map((pkm) => <PokeGridElement key={pkm.id} pkm={pkm} />);
+
+  const handleClick = () => {
+    setCtxPokedex((prev) => ({
+      ...prev,
+      class: {
+        ...prev.class,
+        generationView: true,
+        pokemonListView: false,
+      }
+    }));
+  }
+
   return (
     <div id="poke-grid-container">
-      <SearchBar searchBarID="poke-grid-search-bar"/>
+      <button type="button" name="back-btn" onClick={handleClick}>
+        <svg viewBox="0 0 24 24"><path fill="currentColor" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" /></svg>
+        GENERATIONS
+      </button>
+      <SearchBar searchBarID="poke-grid-search-bar" />
       <ol id="poke-grid">
         {pokeData}
       </ol>

73d966242f581b2e5bbab1ab36dc1c7fb0f42cd6
Nick On <nick.on437@gmail.com>
2020-11-27 21:28:38 +1100

    New - Set logic to load pokeentry dynamically

--- src/components/PokeEntry.js
+++ src/components/PokeEntry.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import StringUtil from '../utils/StringUtil';
 
 const PokeEntry = ({ pkmSpecies }) => {
-  console.log('pkmSpecies', pkmSpecies);
+  const [pokeEntry, setPokeEntry] = useState(null);
+  const versionSelectRef = useRef(null);
+
   const pokemonVersions = [
     ['red', 'blue', 'yellow'],
     ['gold', 'silver', 'crystal'],
@@ -20,39 +22,48 @@ const PokeEntry = ({ pkmSpecies }) => {
     ['sword', 'shield'],
   ];
 
-  const availableEntries = {};
+  let availableEntries = {};
 
-  const pokeEntry = () => {
-    if (pkmSpecies) {
-      let entry = pkmSpecies.flavor_text_entries.find((entry) => (entry.language.name === 'en')).flavor_text;
-      return entry.replace('', ' '); // Clean up entry text
-    }
+  const generateOptionsJsx = () => {
+    availableEntries = {}
+    
+    const genOptsJsx = pokemonVersions.map((gen, index) => {
+      if (pkmSpecies) {
+        const optionsJsx = gen.map((version) => {
+          let entry = pkmSpecies.flavor_text_entries.find((entry) => (entry.language.name === 'en' && entry.version.name === version));
+          if (entry) {
+            availableEntries[version] = entry;
+            return (<option value={version}>{StringUtil.cleanUpString(version)}</option>);
+          }
+          return;
+        });
+
+        if (optionsJsx) {
+          return (<optgroup label={`GENERATION ${StringUtil.convertToRoman(index + 1)}`}>
+            {optionsJsx}
+          </optgroup>);
+        }
+      }
+
+      return;
+    });
+
+    return genOptsJsx;
+  }
+
+  const handleChangeVersion = () => {
+    setPokeEntry(availableEntries[versionSelectRef.current.value].flavor_text.replace('', ' '));
   };
 
-  const genOptsJsx = pokemonVersions.map((gen, index) => {
-    if (pkmSpecies) {
-      const optionsJsx = gen.map((version) => {
-        let entry = pkmSpecies.flavor_text_entries.find((entry) => (entry.language.name === 'en' && entry.version.name === version));
-        if (entry) {
-          availableEntries[version] = entry;
-          return (<option value={version}>{StringUtil.cleanUpString(version)}</option>);
-        }
-        return;
-      });
-
-      if (optionsJsx) {
-        return (<optgroup label={`GENERATION ${StringUtil.convertToRoman(index + 1)}`}>
-          {optionsJsx}
-        </optgroup>);
-      }
-    }
-    return;
-  });
+  useEffect(() => {
+    console.log('useEffect');
+    handleChangeVersion();
+  }, [pkmSpecies]);
 
   return (
     <section id="pokedex-entry-section">
-      <h2>Pokedex entry <select>{genOptsJsx}</select></h2>
-      <div>{pokeEntry()}</div>
+      <h2>Pokedex entry <select ref={versionSelectRef} onChange={handleChangeVersion}>{generateOptionsJsx()}</select></h2>
+      <div>{pokeEntry}</div>
     </section>
   )
 }
